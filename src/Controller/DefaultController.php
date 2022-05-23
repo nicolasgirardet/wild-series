4bf9df1d5aa5d6191ebd45729e37a1edc86bb4cb
Nicolas Girardet <nicolasgirardet@live.fr>
2022-05-23 20:05:48 +0200

    commit for quest 2

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+// src/Controller/DefaultController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class DefaultController extends AbstractController
+
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+
+    {
+            return $this->render('index.html.twig');
+
+    }
+
+}
